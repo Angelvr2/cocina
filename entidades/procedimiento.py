f56b019b0c5331a5a6e436636b9dd9e4161ad108
chavarebollo25@gmail.com <144269997+ItsGalz@users.noreply.github.com>
2026-02-12 11:23:21 -0700

    cocina nueva

---
+++ entidades/procedimiento.py
@@ -0,0 +1,7 @@
+class Procedimiento:
+    
+    def __init__(self, orden, descripcion):
+        self.orden = orden
+        self.descripcion = descripcion
+        
+        
---
+++ entidades/receta.py
@@ -0,0 +1,7 @@
+class Receta:
+    
+    def __init__(self, nombre, porciones, ingredientes, procedimientos):
+        self.nombre = nombre
+        self.porciones = porciones
+        self.ingredientes = ingredientes
+        self.procedimientos = procedimientos
