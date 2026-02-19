657a9575247aacd20899fb2085d818bc11e4f7cc
chavarebollo25@gmail.com <144269997+ItsGalz@users.noreply.github.com>
2026-02-19 11:25:58 -0700

    Se agrego metodo distancia

--- entidades/procedimiento.py
+++ entidades/procedimiento.py
@@ -1,6 +1,6 @@
 class Procedimiento:
     
-    def __init__(self, orden, descripcion):
+    def __init__(self, orden: int, descripcion: str):
         self.orden = orden
         self.descripcion = descripcion
         
